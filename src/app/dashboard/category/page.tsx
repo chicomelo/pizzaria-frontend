be83cfa08134ee8a2e9bd5d297dd2ebf622916e9
Joao Francisco <jfpmelo@gmail.com>
2025-05-21 19:16:40 -0300

    fix: deploy

--- src/app/dashboard/category/page.tsx
+++ src/app/dashboard/category/page.tsx
@@ -2,7 +2,7 @@ import styles from './styles.module.scss'
 import { FormCategory } from './FormCategory'
 import { handleRegisterCategory } from './actions'
 
-
+//export default function Category(formData: FormData){
 export default function Category(){
 
     return(
